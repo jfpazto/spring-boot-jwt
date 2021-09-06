732925564bd27d15f8e22acffb7a49b9e189c8dd
jfpazto <jfpazto@hotmail.com>
2021-09-06 18:00:57 +0100

    Segunda parte terminada

--- src/main/java/org/ada/school/config/SecurityConfiguration.java
+++ src/main/java/org/ada/school/config/SecurityConfiguration.java
@@ -21,6 +21,7 @@ public class SecurityConfiguration
         http.authorizeRequests()
                 .antMatchers( HttpMethod.GET, "/v1/health" ).permitAll()
                 .antMatchers( HttpMethod.POST,"/v1/auth" ).permitAll()
+                .antMatchers( HttpMethod.POST,"/v1/user" ).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS )
